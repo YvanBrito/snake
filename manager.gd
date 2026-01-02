2c74a8a10eb006fd49027cda384b2214876af99c
Yvan Brito <yvanbrito96@gmail.com>
2026-01-01 21:58:35 -0300

    grid e melhora no input/atualizacao do jogo

--- manager.gd
+++ manager.gd
@@ -2,16 +2,38 @@ extends Node2D
 class_name Manager
 
 const FOOD = preload("uid://cihptc0jq45je")
+const GRID = preload("uid://3f1tokgmmxxq")
 var food: Area2D
 var paused: bool = false
 
+var piece_size: int = 20
+
+var field_height: int = 10
+var field_length: int = 15
+
+var upper_limit: int = -(field_height * piece_size)
+var bottom_limit: int = field_height * piece_size
+var left_limit: int = -(field_length * piece_size)
+var right_limit: int = (field_length * piece_size)
+
 func _ready() -> void:
 	randomize()
+	
+	create_grid()
+	
 	food = FOOD.instantiate()
-	var rx = rand_multiple_of_20(-560, 560)
-	var ry = rand_multiple_of_20(-310, 310)
+	var rx = rand_multiple_of_20(left_limit, right_limit)
+	var ry = rand_multiple_of_20(upper_limit, bottom_limit)
 	food.position = Vector2(rx, ry)
 	add_child(food)
+
+func create_grid() -> void:
+	for i in range(-field_height, field_height+1):
+		for j in range(-field_length,field_length+1):
+			var grid = GRID.instantiate()
+			grid.position = Vector2(20*j,20*i)
+			grid.z_index = -1
+			add_child(grid)
 
 func rand_multiple_of_20(min_int: int, max_int: int) -> int:
 	# Garante ordem
@@ -38,6 +60,6 @@ func _input(event: InputEvent) -> void:
 
 func food_eaten() -> void:
 	if food:
-		var rx = rand_multiple_of_20(-560, 560)
-		var ry = rand_multiple_of_20(-310, 310)
+		var rx = rand_multiple_of_20(left_limit, right_limit)
+		var ry = rand_multiple_of_20(upper_limit, bottom_limit)
 		food.position = Vector2(rx, ry)
--- snake.gd
+++ snake.gd
@@ -2,60 +2,78 @@ class_name Snake
 extends Node2D
 
 const PIECE = preload("uid://bjpvdais1vc57")
-@onready var timer: Timer = $"../WalkSnakeTime"
+const HEAD = preload("uid://yba2sslesdq2")
 @onready var area_2d: Area2D = $Area2D
+@onready var _timer: Timer = $"../WalkSnakeTime"
 @onready var manager: Manager = $".."
 
 var pieces: Array[Node2D]
 var direction: Vector2 = Vector2.RIGHT
 var last_position: Vector2
+var local_when_input: Vector2 = Vector2.ZERO
+var intervalo_ms: int = 100
 
 func _ready() -> void:
-	timer.timeout.connect(_on_timeout)
+	_timer.timeout.connect(_on_timeout)
 	start_new_game()
 
 func start_new_game() -> void:
-	timer.start()
 	if pieces.size() > 0:
 		for i in range(0, pieces.size()):
 			pieces[i].queue_free()
 	pieces = []
-	var piece = PIECE.instantiate()
-	piece.position = Vector2.ZERO
-	pieces.push_back(piece)
-	add_child(piece)
+	var head = HEAD.instantiate()
+	head.position = Vector2.ZERO
+	pieces.push_back(head)
+	add_child(head)
 	manager.food_eaten()
 
-func _on_timeout() -> void:	
+func _on_timeout() -> void:
 	last_position = pieces[0].position
-	pieces[0].position += direction * 20
-	if pieces[0].position.x > 560:
-		pieces[0].position.x = -560
-	if pieces[0].position.x < -560:
-		pieces[0].position.x = 560
-	if pieces[0].position.y > 300:
-		pieces[0].position.y = -300
-	if pieces[0].position.y < -300:
-		pieces[0].position.y = 300
+	pieces[0].position += direction * manager.piece_size
+	if pieces[0].position.x > manager.right_limit:
+		pieces[0].position.x = manager.left_limit
+	if pieces[0].position.x < manager.left_limit:
+		pieces[0].position.x = manager.right_limit
+	if pieces[0].position.y > manager.bottom_limit:
+		pieces[0].position.y = manager.upper_limit
+	if pieces[0].position.y < manager.upper_limit:
+		pieces[0].position.y = manager.bottom_limit
 	area_2d.position = pieces[0].position
+	if direction == Vector2.LEFT:
+		pieces[0].rotation_degrees = 0
+	if direction == Vector2.UP:
+		pieces[0].rotation_degrees = 90
+	if direction == Vector2.RIGHT:
+		pieces[0].rotation_degrees = 180
+	if direction == Vector2.DOWN:
+		pieces[0].rotation_degrees = 270
 	for i in range(1, pieces.size()):
 		var aux = pieces[i].position
 		pieces[i].position = last_position
 		last_position = aux
 		if pieces[0].position == pieces[i].position:
-			timer.stop()
 			start_new_game()
 			break
 	
 func _input(event: InputEvent) -> void:
-	if (direction == Vector2.UP or direction == Vector2.DOWN) and event.is_action_pressed("ui_left"):
-		direction = Vector2.LEFT
-	elif (direction == Vector2.UP or direction == Vector2.DOWN) and event.is_action_pressed("ui_right"):
-		direction = Vector2.RIGHT
-	elif (direction == Vector2.LEFT or direction == Vector2.RIGHT) and event.is_action_pressed("ui_up"):
-		direction = Vector2.UP
-	elif (direction == Vector2.LEFT or direction == Vector2.RIGHT) and event.is_action_pressed("ui_down"):
-		direction = Vector2.DOWN
+	if event.is_pressed() and not event.is_echo():
+		if (direction == Vector2.UP or direction == Vector2.DOWN) and event.is_action_pressed("ui_left"):
+			_timer.start()
+			_on_timeout()
+			direction = Vector2.LEFT
+		elif (direction == Vector2.UP or direction == Vector2.DOWN) and event.is_action_pressed("ui_right"):
+			_timer.start()
+			_on_timeout()
+			direction = Vector2.RIGHT
+		elif (direction == Vector2.LEFT or direction == Vector2.RIGHT) and event.is_action_pressed("ui_up"):
+			_timer.start()
+			_on_timeout()
+			direction = Vector2.UP
+		elif (direction == Vector2.LEFT or direction == Vector2.RIGHT) and event.is_action_pressed("ui_down"):
+			_timer.start()
+			_on_timeout()
+			direction = Vector2.DOWN
 
 
 func _on_area_2d_area_entered(area: Area2D) -> void:
